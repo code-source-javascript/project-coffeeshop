84f98e2356ab07bc4350fe21c8a51ed99d9b79d8
CalebOkyere <agyakojo4321@gmail.com>
2022-09-20 11:34:42 +0000

    backend almost done

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 import logging
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-42i-issf.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'coffeeshop'
 
 # AuthError Exception
 '''
